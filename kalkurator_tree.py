8abb226b289feb619a03cf0bfebabd604b52a268
Jakob Kenda <kenda.jakob@gmail.com>
2022-07-23 14:01:13 +0200

    dodan kalkulator, ki pred računanjem zgradi drevo

--- kalkurator_tree.py
+++ kalkurator_tree.py
@@ -1,17 +1,48 @@
 #!/usr/bin/python3
 
+from typing import TypeVar
+from collections.abc import Callable
 from operator import add, sub, mul, truediv
+
+operatorji_r = { add: '+', sub: '-', mul: '*', truediv: '/', pow: '^' }
+
+TTree = TypeVar("TTree", bound="Tree")
+
+class Tree:
+	data: Callable[[float, float], float] | float
+	l: TTree | None
+	r: TTree | None
+
+	def __init__(self, data, l = None, r = None) -> None:
+		self.data = data
+		self.l = l
+		self.r = r
+
+	def print(self, globina: int = 0):
+		if type(self.data) is float:
+			print(globina * "  " + str(self.data))
+		else:
+			print(globina * "  " + operatorji_r[self.data])
+			self.l.print(globina + 1)
+			self.r.print(globina + 1)
+
+	def ovrednoti(self) -> float:
+		if type(self.data) == float:
+			return self.data
+		else:
+			return self.data(self.l.ovrednoti(), self.r.ovrednoti())
 
 operatorji = { '+': add, '-': sub, '*': mul, '/': truediv, '^': pow }
 ignore = { '+', '-', '*', '/', '^', ')' }
-
 
 def main():
 	izraz = "\0"
 	while izraz != "":
 		try:
 			izraz = input("> ")
-			print("=", ovrednoti(izraz))
+			tree = izgradi(izraz)
+			tree.print()
+			print("\n=", tree.ovrednoti())
 		except KeyboardInterrupt:
 			print()
 			exit()
@@ -19,9 +50,8 @@ def main():
 			print(e)
 
 
-def ovrednoti(izraz: str) -> float:
+def izgradi(izraz: str) -> Tree:
 	predprocesiran_izraz = predprocesiran(izraz)
-	print(predprocesiran_izraz)
 	return aditivni(predprocesiran_izraz)
 
 def predprocesiran(izraz: str) -> str:
@@ -35,11 +65,13 @@ def predprocesiran(izraz: str) -> str:
 		prev = predproc_str[i-1]; curr = predproc_str[i]
 		if prev.isnumeric() and not curr.isnumeric() and not curr in ignore:
 			predproc_str = predproc_str[:i] + '*' + predproc_str[i:]
+		elif prev == ')' and curr == '(':
+			predproc_str = predproc_str[:i] + '*' + predproc_str[i:]
 		i += 1
 
 	return predproc_str
 
-def aditivni(izraz: str) -> float:
+def aditivni(izraz: str) -> Tree:
 	plus  = poišči(izraz, '+')
 	minus = poišči(izraz, '-')
 
@@ -48,7 +80,7 @@ def aditivni(izraz: str) -> float:
 		return multiplikativni(izraz)
 	elif plus > minus:
 		# '+' ima prednost
-		return aditivni(izraz[:plus]) + multiplikativni(izraz[plus+1:])
+		return Tree(add, aditivni(izraz[:plus]), multiplikativni(izraz[plus+1:]))
 	elif minus > plus:
 		# '-' ima prednost 
 		if minus == 0:
@@ -56,10 +88,10 @@ def aditivni(izraz: str) -> float:
 			return multiplikativni(izraz)
 		elif izraz[minus-1] in operatorji:
 			# negacija
-			return operatorji[izraz[minus-1]](aditivni(izraz[:minus-1]), multiplikativni(izraz[minus:]))
+			return Tree(operatorji[izraz[minus-1]], aditivni(izraz[:minus-1]), multiplikativni(izraz[minus:]))
 		else:
 			# odštevanje
-			return aditivni(izraz[:minus]) - multiplikativni(izraz[minus+1:])
+			return Tree(sub, aditivni(izraz[:minus]), multiplikativni(izraz[minus+1:]))
 
 def multiplikativni(izraz: str) -> float:
 	krat    = poišči(izraz, '*')
@@ -70,30 +102,29 @@ def multiplikativni(izraz: str) -> float:
 		return potenčni(izraz)
 	elif krat > deljeno:
 		# '*' ima prednost
-		return multiplikativni(izraz[:krat]) * potenčni(izraz[krat+1:])
+		return Tree(mul, multiplikativni(izraz[:krat]), potenčni(izraz[krat+1:]))
 	elif deljeno > krat:
 		# '/' ima prednost
-		return multiplikativni(izraz[:deljeno]) / potenčni(izraz[deljeno+1:])
+		return Tree(truediv, multiplikativni(izraz[:deljeno]), potenčni(izraz[deljeno+1:]))
 
-def potenčni(izraz: str) -> float:
+def potenčni(izraz: str) -> Tree:
 	na = poišči(izraz, '^')
 
 	if na == -1:
 		# ni znaka '^'
 		return osnovni(izraz)
 	else:
-		return potenčni(izraz[:na]) ** potenčni(izraz[na+1:])
+		return Tree(pow, potenčni(izraz[:na]), potenčni(izraz[na+1:]))
 
-def osnovni(izraz: str) -> float:
-	# print(izraz)
+def osnovni(izraz: str) -> Tree:
 	if len(izraz) == 0:
-		return 0
-	if izraz[0] == '(':
+		return Tree(0.0)
+	if izraz[0] == '(' and izraz[-1] == ')':
 		return aditivni(izraz[1:-1])
 
-	return float(izraz)
+	return Tree(float(izraz))
 
-def poišči(izraz: str, char: str) -> int:
+def poišči(izraz: str, niz: str) -> int:
 	oklepajev = 0
 
 	i = len(izraz) - 1
@@ -103,7 +134,7 @@ def poišči(izraz: str, char: str) -> int:
 		elif izraz[i] == '(':
 			oklepajev -= 1
 
-		if oklepajev == 0 and izraz[i] == char:
+		if oklepajev == 0 and izraz[i:].startswith(niz):
 			# iskani znak ni znotraj oklepajev
 			return i
 		i -= 1
--- sestevanje.py
+++
@@ -1,4 +0,0 @@
-st1 = input()
-st2 = input()
-st3 = int(st1) + int(st2)
-print(str(st3))
